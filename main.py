6ac82937bcf0d4b9088eb900a413bca66eeb357a
juan <juanfergp00@gmail.com>
2025-02-25 20:59:54 -0500

    feat se crea menu en el main.py

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
 #software estructuras
-nombreVendedor="Juan"
+nombreVendedor=None
+productos=[]#arreglo, lista
+producto={}#objetos, diccionario
+
+#creando un ciclo en python
+opcion=None
+print("***Merqueo APP***")
+print("1. Crear lista de mercado")
+print("5. Salir")
+
+while opcion != 5:
+    print("dentro del while indentacion")
+    opcion=int(input("Digita una opcion: "))
+
+
